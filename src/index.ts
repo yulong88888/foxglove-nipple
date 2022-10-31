5d6041418f170ceb01d36e29fed84965489bb02b
GuYuLong <1327417509@qq.com>
2022-10-31 22:55:12 +0800

    更新名称

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { ExtensionContext } from "@foxglove/studio";
 import { initExamplePanel } from "./ExamplePanel";
 
 export function activate(extensionContext: ExtensionContext): void {
-  extensionContext.registerPanel({ name: "Nipple", initPanel: initExamplePanel });
+  extensionContext.registerPanel({ name: "Virtual Joystick", initPanel: initExamplePanel });
 }
