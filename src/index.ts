5567a1eef2f4c7a211eb707e27afb330e8188ffb
Troy Gibb <troy.gibb@gmail.com>
2024-05-01 08:57:58 -0700

    Fix name

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { ExtensionContext } from "@foxglove/studio";
 import { initExamplePanel } from "./VirtualJoystickPanel";
 
 export function activate(extensionContext: ExtensionContext): void {
-  extensionContext.registerPanel({ name: "Virtual Joystick [fix]", initPanel: initExamplePanel });
+  extensionContext.registerPanel({ name: "Virtual Joystick", initPanel: initExamplePanel });
 }
